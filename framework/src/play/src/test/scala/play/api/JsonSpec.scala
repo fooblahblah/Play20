e909feadedddcb87aae4f2bcb3bae30487dae701
Jeff Simpson <jeff.simpson@livingsocial.com>
2012-08-23 14:55:27 -0600

    Cherry picked Json NaN functionality

--- framework/src/play/src/test/scala/play/api/JsonSpec.scala
+++ framework/src/play/src/test/scala/play/api/JsonSpec.scala
@@ -124,6 +124,14 @@ object JsonSpec extends Specification {
       val expectedJson = JsArray(List(JsNull))
       parsedJson must equalTo(expectedJson)
     }
+    "Can parse Array containing NaN as null" in {
+      val js = Json.parse("""[1.0, 2.0, NaN]""")
+      js === JsArray(JsNumber(1.0) :: JsNumber(2.0) :: JsNull :: Nil)
+    }
+    "Can parse bare NaN as null" in {
+      val js = Json.parse("""NaN""")
+      js === JsNull
+    }
   }
 
 }
